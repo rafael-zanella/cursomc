f91d6933e6f59b63ecf1e34cb7b4ca9388e2f24e
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-04-06 14:54:01 -0300

    feat(Categoria): add service and repository.

---
+++ src/main/java/com/zanella/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.zanella.cursomc.services;
+
+import com.zanella.cursomc.domain.Categoria;
+import com.zanella.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
