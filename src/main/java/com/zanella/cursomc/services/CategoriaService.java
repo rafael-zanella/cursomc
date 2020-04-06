98ad96eb5bddc5e968b61f0e085f33239888b852
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-04-06 19:58:29 -0300

    fix(CategoriaService, CategoriaResource): change 'buscar' method to 'find'.

--- src/main/java/com/zanella/cursomc/services/CategoriaService.java
+++ src/main/java/com/zanella/cursomc/services/CategoriaService.java
@@ -2,8 +2,11 @@ package com.zanella.cursomc.services;
 
 import com.zanella.cursomc.domain.Categoria;
 import com.zanella.cursomc.repositories.CategoriaRepository;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.Optional;
 
@@ -13,7 +16,7 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repository;
 
-    public Categoria buscar(Integer id) {
+    public Categoria find(Integer id) {
         Optional<Categoria> obj = repository.findById(id);
         return obj.orElse(null);
     }
