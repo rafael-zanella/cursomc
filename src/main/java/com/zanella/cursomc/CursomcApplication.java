99901c8376200498bbd14b7630bf606ddb0d557f
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-04-06 19:27:26 -0300

    feat(Produto, Categoria): add produto entity and many to many relationship between produto_categoria.

--- src/main/java/com/zanella/cursomc/CursomcApplication.java
+++ src/main/java/com/zanella/cursomc/CursomcApplication.java
@@ -1,7 +1,9 @@
 package com.zanella.cursomc;
 
 import com.zanella.cursomc.domain.Categoria;
+import com.zanella.cursomc.domain.Produto;
 import com.zanella.cursomc.repositories.CategoriaRepository;
+import com.zanella.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,6 +18,9 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -25,8 +30,18 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 
-//		categoriaRepository.save(cat1);
-//		categoriaRepository.save(cat2);
+		Produto p1 = new Produto(null, "computador", 2000.00);
+		Produto p2 = new Produto(null, "impressora", 800.00);
+		Produto p3 = new Produto(null, "mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
---
+++ src/main/java/com/zanella/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.zanella.cursomc.repositories;
+
+import com.zanella.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
