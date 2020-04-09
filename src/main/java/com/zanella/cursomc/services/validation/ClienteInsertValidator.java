dda7f7812798acd1d4eaa85be0989dcb5afa9567
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-04-09 18:36:59 -0300

    feat(validation, ClienteNewDTO): create ClienteInsert validator.

---
+++ src/main/java/com/zanella/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,39 @@
+package com.zanella.cursomc.services.validation;
+
+import com.zanella.cursomc.domain.enums.TipoCliente;
+import com.zanella.cursomc.dto.ClienteNewDTO;
+import com.zanella.cursomc.resources.exceptions.FieldMessage;
+import com.zanella.cursomc.services.validation.utils.BR;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Override
+    public void initialize(ClienteInsert constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+        }
+
+        if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        for(FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
