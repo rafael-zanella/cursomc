492e78512f833a887ceb36b4baced683a2c5bf9d
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-04-08 23:35:16 -0300

    feat(CategoriaDTO, CategoriaResource, CategoriaService): add bean validation.

--- src/main/java/com/zanella/cursomc/services/CategoriaService.java
+++ src/main/java/com/zanella/cursomc/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.zanella.cursomc.services;
 
 import com.zanella.cursomc.domain.Categoria;
+import com.zanella.cursomc.dto.CategoriaDTO;
 import com.zanella.cursomc.repositories.CategoriaRepository;
 import com.zanella.cursomc.services.exceptions.DataIntegrityException;
 import com.zanella.cursomc.services.exceptions.ObjectNotFoundException;
@@ -50,5 +51,9 @@ public class CategoriaService {
     public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
         return repository.findAll(pageRequest);
+    }
+
+    public Categoria fromDTO(CategoriaDTO objDto) {
+        return new Categoria(objDto.getId(), objDto.getNome());
     }
 }
