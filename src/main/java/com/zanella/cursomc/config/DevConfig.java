5fb8a534f7a0ca9689c93c7f62032459e13e4d0f
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-04-13 16:14:18 -0300

    feat(SmtpEmailService, application-dev): send email through google server.

--- src/main/java/com/zanella/cursomc/config/DevConfig.java
+++ src/main/java/com/zanella/cursomc/config/DevConfig.java
@@ -1,6 +1,8 @@
 package com.zanella.cursomc.config;
 
 import com.zanella.cursomc.services.DBService;
+import com.zanella.cursomc.services.EmailService;
+import com.zanella.cursomc.services.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +30,11 @@ public class DevConfig {
 
         dbService.instantiateTestDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService emailService() {
+        return new SmtpEmailService();
     }
 
 }
