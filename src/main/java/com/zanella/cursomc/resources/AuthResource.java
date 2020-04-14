b30950f0359ae42fea5da5f8848e700c324fd919
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-04-14 04:01:36 -0300

    feat(EmailDTO, AuthService, EmailService, AuthResource): add forgot pass feature.

--- src/main/java/com/zanella/cursomc/resources/AuthResource.java
+++ src/main/java/com/zanella/cursomc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.zanella.cursomc.resources;
 
+import com.zanella.cursomc.dto.EmailDTO;
 import com.zanella.cursomc.security.JWTUtil;
 import com.zanella.cursomc.security.UserDetailsImpl;
+import com.zanella.cursomc.services.AuthService;
 import com.zanella.cursomc.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/auth")
@@ -18,11 +22,20 @@ public class AuthResource {
     @Autowired
     private JWTUtil jwtUtil;
 
+    @Autowired
+    private AuthService authService;
+
     @PostMapping(value = "/refresh_token")
     public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
         UserDetailsImpl user = UserService.authenticated();
         String token = jwtUtil.generateToken(user.getUsername());
         response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping(value = "/forgot")
+    public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDTO) {
+        authService.sendNewPassword(objDTO.getEmail());
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/zanella/cursomc/services/EmailService.java
+++ src/main/java/com/zanella/cursomc/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.zanella.cursomc.services;
 
+import com.zanella.cursomc.domain.Cliente;
 import com.zanella.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
@@ -8,4 +9,6 @@ public interface EmailService {
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
