81a2c56e81ce25cfcfe10ca748264ca756430a5a
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-04-14 23:30:14 -0300

    feat(S3Config, S3Service): add aws s3 for saving images.

--- src/main/java/com/zanella/cursomc/CursomcApplication.java
+++ src/main/java/com/zanella/cursomc/CursomcApplication.java
@@ -1,5 +1,7 @@
 package com.zanella.cursomc;
 
+import com.zanella.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,13 +11,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
-
+		s3Service.uploadFile("C:\\Users\\Zanella\\Desktop\\yellow-phone.jpg");
 	}
 }
